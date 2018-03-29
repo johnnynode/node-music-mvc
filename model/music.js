d2b32252803a5a7e549a86ab6156f3d198eed758
johnnynode <johnnynode@gmail.com>
2018-03-30 06:59:58 +0800

    model music transfrom to ES6

--- model/music.js
+++ model/music.js
@@ -2,19 +2,21 @@
 
 let musicList = require('./data');
 
-const Music = function(id, name, singer, isHightRate){
-  this.id = id;
-  this.name = name;
-  this.singer = singer;
-  this.isHightRate = isHightRate;
-};
-Music.prototype = {
+class Music {
+  constructor(id, name, singer, isHightRate) {
+    this.id = id;
+    this.name = name;
+    this.singer = singer;
+    this.isHightRate = isHightRate;
+  }
+
   // 获取所有音乐
-  getAllMusic:function () {
+  getAllMusic() {
     return musicList;
-  },
+  }
+
   // 添加一首音乐
-  addMusic:function(name, singer, isHightRate) {
+  addMusic(name, singer, isHightRate) {
     let id = musicList[musicList.length - 1].id - 0 + 1;
     let json = {
       id,
@@ -30,9 +32,10 @@ Music.prototype = {
       console.log('add error');
     }
     return flag;
-  },
+  }
+
   // 编辑一首音乐
-  editMusicById:function (id, name, singer, isHightRate) {
+  editMusicById(id, name, singer, isHightRate) {
     // 根据id查找数组中的索引
     let index = musicList.findIndex(m => m.id === id);
     if(index === -1) {
@@ -42,22 +45,25 @@ Music.prototype = {
     musicList[index].singer = singer;
     musicList[index].isHightRate = isHightRate === '1';
     return true;
-  },
+  }
+
   // 通过id删除音乐
-  removeMusicById:function (id) {
+  removeMusicById(id) {
     let index = musicList.findIndex(m => m.id === id);
     if(index === -1) {
       return false;
     }
     musicList.splice(index, 1);
     return true;
-  },
+  }
+
   // 通过id查询音乐
-  getMusicById:function (id) {
+  getMusicById(id) {
     return musicList.find((item) => {
       return item.id === id
     });
   }
+
 }
 
 module.exports = new Music();
